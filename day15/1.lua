cbd7ba304c997396a5de04b16cdd250b1c937cab
Bart Stavenuiter <spoiker@hotmail.com>
2021-12-16 14:12:37 +0100

    Track visited and nodes on demain, no prepopulation

--- day15/1.lua
+++ day15/1.lua
@@ -17,18 +17,8 @@ for k,line in pairs(lines) do
     positionTable[k] = ExplodeLine(line, '.')
 end
 
+local nodes = {{0}}
 local visited = {}
-local nodes = {}
-for i = 1,#positionTable do
-    visited[i] = {}
-    nodes[i] = {}
-    for j = 1,#positionTable do
-        visited[i][j] = false
-        nodes[i][j] = math.huge
-    end
-end
-
-nodes[1][1] = 0
 local steps = 0
 local currentNode = {x=1,y=1,v=0}
 local length = #positionTable
@@ -40,9 +30,21 @@ repeat
         {x=currentNode.x,y=currentNode.y-1}
     }
     for _,v in ipairs(neighbours) do
-        if visited[v.y] ~= nil and visited[v.y][v.x] == false then
-            nodes[v.y][v.x] = math.min(currentNode.v + positionTable[v.y][v.x], nodes[v.y][v.x])
+        if v.y > 0 and v.y <= length and v.x > 0 and v.x <= length then
+            if visited[v.y] == nil then
+                visited[v.y] = {}
+            end
+            if visited[v.y][v.x] == nil then
+                if nodes[v.y] == nil then
+                    nodes[v.y] = {}
+                end
+                nodes[v.y][v.x] = math.min(currentNode.v + positionTable[v.y][v.x], nodes[v.y][v.x] and nodes[v.y][v.x] or math.huge)
+            end
         end
+    end
+
+    if visited[currentNode.y] == nil then
+        visited[currentNode.y] = {}
     end
 
     visited[currentNode.y][currentNode.x] = true
@@ -51,14 +53,14 @@ repeat
     local smallest = math.huge
     for k,v in pairs(nodes) do
         for j,s in pairs(v) do
-            if visited[k][j] == false and smallest > s then
+            if visited[k][j] == nil and smallest > s then
                 smallest = s
                 smallestIndex = {x=j,y=k,v=s}
             end
         end
     end
 
-    if visited[length][length] or not smallestIndex then
+    if (visited[length] and visited[length][length]) or not smallestIndex then
         break
     end
     --print('new smallest index ' .. smallestIndex.y .. '-' .. smallestIndex.x)
@@ -66,6 +68,6 @@ repeat
     steps = steps+1
 until steps ==10000
 
-if visited[length][length] then
+if visited[length] and visited[length][length] then
     print('Shortest path to end ' .. nodes[length][length])
 end
